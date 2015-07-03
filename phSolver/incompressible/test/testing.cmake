41f582925dd9e500cf7fd84d7a79470d906f14cf
Cameron Smith <smithc11@rpi.edu>
2015-07-02 22:01:36 -0400

    helps to have the test files

---
+++ phSolver/incompressible/test/testing.cmake
@@ -0,0 +1,17 @@
+set(CASES "/path/to/test/cases/"
+  CACHE string
+  "path to the test cases")
+
+
+set(CDIR ${CASES}/crossflow)
+add_test(copyInpCfg 
+  cp ${CMAKE_SOURCE_DIR}/phSolver/common/input.config ${CDIR})
+add_test(resetNumStart 
+  cp ${CDIR}/numstart.dat ${CDIR}/4-procs_case/numstart.dat)
+add_test(
+  NAME crossflow
+  COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${CMAKE_BINARY_DIR}/bin/phastaIC.exe
+  WORKING_DIRECTORY ${CASES}/crossflow
+)
+add_test(compareCrossflow 
+  diff ${CDIR}/restart-dat.4.1.ref ${CDIR}/4-procs_case/restart-dat.4.1)
