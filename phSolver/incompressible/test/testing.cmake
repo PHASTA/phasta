b3c8e34352159d3780c8ab2989229f360d50ee37
Cameron Smith <smithc11@rpi.edu>
2015-07-07 09:41:23 -0400

    changed crossflow test case directory name to incompressible

--- phSolver/incompressible/test/testing.cmake
+++ phSolver/incompressible/test/testing.cmake
@@ -1,12 +1,12 @@
-set(CDIR ${CASES}/crossflow)
-add_test(copyInpCfg 
+set(CDIR ${CASES}/incompressible)
+add_test(copyInpCfg
   cp ${PHASTA_SOURCE_DIR}/phSolver/common/input.config ${CDIR})
-add_test(resetNumStart 
+add_test(resetNumStart
   cp ${CDIR}/numstart.dat ${CDIR}/4-procs_case/numstart.dat)
 add_test(
-  NAME crossflow
+  NAME incompressible
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${PHASTA_BINARY_DIR}/bin/phastaIC.exe
-  WORKING_DIRECTORY ${CASES}/crossflow
+  WORKING_DIRECTORY ${CASES}/incompressible
 )
-add_test(compareCrossflow 
+add_test(compareIncompressible
   diff ${CDIR}/restart-dat.4.1.ref ${CDIR}/4-procs_case/restart-dat.4.1)
