164f518b06a8bcf2695935d23d516f5fb9f40812
Cameron Smith <smithc11@rpi.edu>
2015-07-15 22:19:59 -0400

    incompressible test case now has both posix and sync io files

--- phSolver/incompressible/test/testing.cmake
+++ phSolver/incompressible/test/testing.cmake
@@ -1,6 +1,8 @@
 set(CDIR ${CASES}/incompressible)
 add_test(copyInpCfg
   cp ${PHASTA_SOURCE_DIR}/phSolver/common/input.config ${CDIR})
+add_test(linkProcsDir
+  ln -s ${CDIR}/4-procs_case-SyncIO-2 ${CDIR}/4-procs_case)
 add_test(resetNumStart
   cp ${CDIR}/numstart.dat ${CDIR}/4-procs_case/numstart.dat)
 add_test(
@@ -9,11 +11,11 @@ add_test(
   WORKING_DIRECTORY ${CASES}/incompressible
 )
 add_test(compareIncompressible
-  diff ${CDIR}/restart-dat.4.1.ref ${CDIR}/4-procs_case/restart-dat.4.1)
+  diff ${CDIR}/restart-dat.4.1.syncio.ref ${CDIR}/4-procs_case/restart-dat.4.1)
 add_test(
   NAME incompressibleRestart
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${PHASTA_BINARY_DIR}/bin/phastaIC.exe
   WORKING_DIRECTORY ${CASES}/incompressible
 )
 add_test(compareIncompressibleRestart
-  diff ${CDIR}/restart-dat.8.1.ref ${CDIR}/4-procs_case/restart-dat.8.1)
+  diff ${CDIR}/restart-dat.8.1.syncio.ref ${CDIR}/4-procs_case/restart-dat.8.1)
