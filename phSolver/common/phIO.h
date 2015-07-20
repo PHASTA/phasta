a3ffe6a5a103bccff134aeedde8158c14521120a
Cameron Smith <smithc11@rpi.edu>
2015-07-20 09:45:26 -0400

    delete commented lines

--- phSolver/common/phIO.h
+++ phSolver/common/phIO.h
@@ -3,22 +3,14 @@
 
 #include <FCMangle.h>
 
-//#define phio_readheader \
-//  FortranCInterface_GLOBAL_(phio_readheader, PHIO_READHEADER)
 #define phio_writeheader \
   FortranCInterface_GLOBAL_(phio_writeheader, PHIO_WRITEHEADER)
-//#define phio_readdatablock \
-//  FortranCInterface_GLOBAL_(phio_readdatablock, PHIO_READDATABLOCK)
 #define phio_writedatablock \
   FortranCInterface_GLOBAL_(phio_writedatablock, PHIO_WRITEDATABLOCK)
-//#define phio_openfile_read \
-//  FortranCInterface_GLOBAL_(phio_openfile_read, PHIO_OPENFILE_READ)
 #define phio_openfile_write \
   FortranCInterface_GLOBAL_(phio_openfile_write, PHIO_OPENFILE_WRITE)
 #define phio_restartname \
   FortranCInterface_GLOBAL_(phio_restartname, PHIO_RESTARTNAME)
-//#define phio_closefile_read \
-//  FortranCInterface_GLOBAL_(phio_closefile_read, PHIO_CLOSEFILE_READ)
 #define phio_closefile_write \
   FortranCInterface_GLOBAL_(phio_closefile_write, PHIO_CLOSEFILE_WRITE)
 
