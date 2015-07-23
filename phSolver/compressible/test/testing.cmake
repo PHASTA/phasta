fe88b52d35537d3736cc3d33d9db7e51353aba7d
Cameron Smith <smithc11@rpi.edu>
2015-07-23 00:45:13 -0400

    adding posix tests and restarts

--- phSolver/compressible/test/testing.cmake
+++ phSolver/compressible/test/testing.cmake
@@ -1,12 +1,32 @@
 set(CDIR ${CASES}/compressible)
 add_test(copyInpCfg 
   cp ${PHASTA_SOURCE_DIR}/phSolver/common/input.config ${CDIR})
-add_test(resetNumStart 
+add_test(compressibleResetNumStart-sync
   cp ${CDIR}/numstart.dat ${CDIR}/2-procs_case/numstart.dat)
 add_test(
-  NAME compressible
+  NAME compressible-sync
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 2 ${PHASTA_BINARY_DIR}/bin/phastaC.exe
   WORKING_DIRECTORY ${CASES}/compressible
 )
-add_test(compareCompressible
+add_test(compareCompressible-sync
   diff ${CDIR}/restart-dat.2.1.ref ${CDIR}/2-procs_case/restart-dat.2.1)
+add_test(
+  NAME compressibleRestart-sync
+  COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 2 ${PHASTA_BINARY_DIR}/bin/phastaC.exe
+  WORKING_DIRECTORY ${CASES}/compressible
+)
+
+add_test(linkProcsDir-posix
+  ln -snf ${CDIR}/2-procs_case-Posix ${CDIR}/2-procs_case)
+add_test(compressibleResetNumStart-posix
+  cp ${CDIR}/numstart.dat ${CDIR}/2-procs_case/numstart.dat)
+add_test(
+  NAME compressible-posix
+  COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 2 ${PHASTA_BINARY_DIR}/bin/phastaC.exe
+  WORKING_DIRECTORY ${CASES}/compressible
+)
+add_test(
+  NAME compressibleRestart-posix
+  COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 2 ${PHASTA_BINARY_DIR}/bin/phastaC.exe
+  WORKING_DIRECTORY ${CASES}/compressible
+)
--- phSolver/incompressible/test/testing.cmake
+++ phSolver/incompressible/test/testing.cmake
@@ -1,21 +1,36 @@
 set(CDIR ${CASES}/incompressible)
 add_test(copyInpCfg
   cp ${PHASTA_SOURCE_DIR}/phSolver/common/input.config ${CDIR})
-add_test(linkProcsDir
+add_test(linkProcsDir-sync
   ln -snf ${CDIR}/4-procs_case-SyncIO-2 ${CDIR}/4-procs_case)
-add_test(resetNumStart
+add_test(incompressibleResetNumStart-sync
   cp ${CDIR}/numstart.dat ${CDIR}/4-procs_case/numstart.dat)
 add_test(
-  NAME incompressible
+  NAME incompressible-sync
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${PHASTA_BINARY_DIR}/bin/phastaIC.exe
   WORKING_DIRECTORY ${CASES}/incompressible
 )
-add_test(compareIncompressible
+add_test(compareIncompressible-sync
   diff ${CDIR}/restart-dat.4.1.syncio.ref ${CDIR}/4-procs_case/restart-dat.4.1)
 add_test(
-  NAME incompressibleRestart
+  NAME incompressibleRestart-sync
   COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${PHASTA_BINARY_DIR}/bin/phastaIC.exe
   WORKING_DIRECTORY ${CASES}/incompressible
 )
-add_test(compareIncompressibleRestart
+add_test(compareIncompressibleRestart-sync
   diff ${CDIR}/restart-dat.8.1.syncio.ref ${CDIR}/4-procs_case/restart-dat.8.1)
+
+add_test(linkProcsDir-posix
+  ln -snf ${CDIR}/4-procs_case-Posix ${CDIR}/4-procs_case)
+add_test(incompressibleResetNumStart-posix
+  cp ${CDIR}/numstart.dat ${CDIR}/4-procs_case/numstart.dat)
+add_test(
+  NAME incompressible-posix
+  COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${PHASTA_BINARY_DIR}/bin/phastaIC.exe
+  WORKING_DIRECTORY ${CASES}/incompressible
+)
+add_test(
+  NAME incompressibleRestart-posix
+  COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} 4 ${PHASTA_BINARY_DIR}/bin/phastaIC.exe
+  WORKING_DIRECTORY ${CASES}/incompressible
+)
