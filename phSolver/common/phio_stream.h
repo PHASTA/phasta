479263023e93481de3d4123fbb7c084588987ee0
Cameron Smith <smithc11@rpi.edu>
2015-08-18 13:30:52 -0400

    starting to implement stream support

---
+++ phSolver/common/phio_stream.h
@@ -0,0 +1,21 @@
+#ifndef PHSOLVER_PHIO_STREAM_H
+#define PHSOLVER_PHIO_STREAM_H
+#include "phio_base.h"
+#include "phstream.h"
+struct streamio_file : phio_file {
+  RStream rs;
+  GRStream grs;
+};
+typedef struct streamio_file* stream_fp;
+void stream_openfile_read(
+    const char filename[],
+    phio_fp fileDescriptor);
+void stream_openfile_write(
+    const char filename[],
+    phio_fp fileDescriptor);
+void stream_closefile(
+    phio_fp fileDescriptor);
+void stream_constructname(
+    const char* in,
+    char* out);
+#endif
--- phSolver/common/streamio.cc
+++ phSolver/common/streamio.cc
@@ -1,11 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "streamio.h"
+#include "phio_stream.h"
+#include "phio_posix.h"
+#include "phio_base.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-void streamio_setup(stream*, phio_fp*) {
-  return;
+static struct phio_ops stream_ops = {
+  stream_openfile_write,
+  stream_closefile,
+  posix_readheader,
+  posix_writeheader,
+  posix_readdatablock,
+  posix_writedatablock,
+  stream_constructname
+};
+
+void streamio_setup(phio_fp*, GRStream grs) {
+  *f = (phio_fp) malloc(sizeof(struct streamio_file));
+  stream_fp sf = (stream_fp) *f;
+  f->ops = &stream_ops;
+  f->file = (int*) malloc(sizeof(int*));
+  f->mode = mode;
+  sf->grs = grs;
 }
-#ifdef __cplusplus
+
+void streamio_setup(phio_fp*, RStream rs) {
+  *f = (phio_fp) malloc(sizeof(struct streamio_file));
+  stream_fp sf = (stream_fp) *f;
+  f->ops = &stream_ops;
+  f->file = (int*) malloc(sizeof(int*));
+  f->mode = mode;
+  sf->rs = rs;
 }
-#endif
