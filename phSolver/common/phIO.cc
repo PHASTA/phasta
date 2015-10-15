322ea7e2cdf8cc0ac9b6d45f0abe611526e08816
Cameron Smith <smithc11@rpi.edu>
2015-10-15 18:30:08 +0000

    io timers

--- phSolver/common/phIO.cc
+++ phSolver/common/phIO.cc
@@ -6,6 +6,28 @@
 #include "phIO.h"
 #include "phComm.h"
 #include "phio_base.h"
+#include <mpi.h>
+
+#ifndef PHASTAIO_TIMERS_ON
+#define PHASTAIO_TIMERS_ON 0
+#endif
+
+namespace {
+  inline double getTime() {
+    return MPI_Wtime();
+  }
+  inline bool isRankZero() {
+    int rank = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    return !rank;
+  }
+  inline printTime(const char* key, double t) {
+#if PHASTAIO_TIMERS_ON==1
+    if( isRankZero() )
+      fprintf(stderr, "%s %f\n", key, t);
+#endif
+  }
+}
 
 #define PHIO_TRACING 0
 namespace {
@@ -26,10 +48,12 @@ void phio_readheader(
     int*  nItems,
     const char  datatype[],
     const char  iotype[] ) {
+  const double t0 = getTime();
   f->ops->readheader(f->file, keyphrase, valueArray,
       nItems, datatype, iotype);
+  printTime(__func__, getTime()-t0);
 }
-void phio_writeheader( 
+void phio_writeheader(
     phio_fp f,
     const char keyphrase[],
     const void* valueArray,
@@ -37,8 +61,10 @@ void phio_writeheader(
     const int* ndataItems,
     const char datatype[],
     const char iotype[] ) {
+  const double t0 = getTime();
   f->ops->writeheader(f->file, keyphrase, valueArray,
       nItems, ndataItems, datatype, iotype);
+  printTime(__func__, getTime()-t0);
 }
 void phio_readdatablock(
     phio_fp f,
@@ -47,8 +73,10 @@ void phio_readdatablock(
     int*  nItems,
     const char  datatype[],
     const char  iotype[] ) {
+  const double t0 = getTime();
   f->ops->readdatablock(f->file, keyphrase, valueArray,
       nItems, datatype, iotype);
+  printTime(__func__, getTime()-t0);
 }
 void phio_writedatablock(
     phio_fp f,
@@ -57,27 +85,35 @@ void phio_writedatablock(
     const int* nItems,
     const char datatype[],
     const char iotype[]) {
+  const double t0 = getTime();
   f->ops->writedatablock(f->file, keyphrase, valueArray,
       nItems, datatype, iotype);
+  printTime(__func__, getTime()-t0);
 }
 
 void phio_constructName(
     phio_fp f,
     const char inName[],
     char* outName) {
+  const double t0 = getTime();
   f->ops->constructname(inName, outName);
+  printTime(__func__, getTime()-t0);
 }
 
 void phio_openfile(
     const char filename[],
     phio_fp f) {
+  const double t0 = getTime();
   trace("openfile",filename,f);
   f->ops->openfile(filename, f);
+  printTime(__func__, getTime()-t0);
 }
 
 void phio_closefile(phio_fp f) {
+  const double t0 = getTime();
   trace("closefile","unknown",f);
   f->ops->closefile(f);
+  printTime(__func__, getTime()-t0);
 }
 
 void phio_appendInt(char* dest, int v) {
