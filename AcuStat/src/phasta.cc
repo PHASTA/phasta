74490262bf7ec67c91f4b0e4ec4855059411bf6e
Ben Matthews <ben@kc2vjw.com>
2016-02-15 15:53:57 -0500

    various utils: include unistd.h for fork() and friends (for debugging)

--- AcuStat/src/phasta.cc
+++ AcuStat/src/phasta.cc
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+
 
 #include <iostream>
 #include <sstream>
--- M2NFixBnd/src/phasta.cc
+++ M2NFixBnd/src/phasta.cc
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
