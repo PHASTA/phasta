302b84e89edd6b5e92f2fe38c7bb6c81f3daaee4
Cameron Smith <smithc11@rpi.edu>
2016-06-22 20:55:55 +0000

    write ilwork data in parallel... in a very crude way

--- phSolver/common/test/phIOreadIlwork.cc
+++ phSolver/common/test/phIOreadIlwork.cc
@@ -9,13 +9,18 @@
 
 int main(int argc, char* argv[]) {
   MPI_Init(&argc,&argv);
-  if( argc != 3 ) {
-    fprintf(stderr, "Usage: %s <geombc posix file> <write ilwork=0|1>\n",argv[0]);
+  int commrank,commsize;
+  MPI_Comm_rank(MPI_COMM_WORLD,&commrank);
+  MPI_Comm_size(MPI_COMM_WORLD,&commsize);
+  if( argc != 4 ) {
+    if( !commrank )
+      fprintf(stderr, "Usage: %s <geombc posix file> <verbosity=0|1|2> <rankoffset>\n",argv[0]);
     MPI_Finalize();
     return 1;
   }
   const char* filename = argv[1];
-  const int writeilwork = atoi(argv[2]);
+  const int verbosity = atoi(argv[2]);
+  const int rankoffset = atoi(argv[3]);
   const char* phrase = "ilwork";
   const char* type = "integer";
   const char* iotype = "binary";
@@ -23,11 +28,16 @@ int main(int argc, char* argv[]) {
   int len = 0;
   int one = 2;
 
+
+  int phastarank = commrank+1+rankoffset;
+
+  char geomfilename[4096];
+  sprintf(geomfilename, "%s/geombc.dat.%d",filename,phastarank);
+
   phio_fp file;
   posixio_setup(&(file), 'r');
-  posix_openfile_single(filename, file);
+  posix_openfile_single(geomfilename, file);
   phio_readheader(file, phrase, &len, &one, type, iotype);
-  fprintf(stderr, "len %d\n", len);
   ilwork = (int*) malloc(len*sizeof(int));
   phio_readdatablock(file, phrase, ilwork, &len, type, iotype);
   phio_closefile(file);
@@ -49,35 +59,42 @@ int main(int argc, char* argv[]) {
     neighbors.insert(iother);
     itkbeg = itkbeg + 4 + 2*numseg;
   }
-  printf("Number of neighboring parts: %d\n",neighbors.size());
-  printf("Number of communication tasks: %d\n",numtask);
-  printf("Number of owner communications: %d\n",numowner);
-  printf("Number of non-owner communications: %d\n",numtask-numowner);
+  if( !commrank )
+      printf("rank peers tasks owned notowned\n");
+  MPI_Barrier(MPI_COMM_WORLD);
+  for(int i=0; i<commsize; i++) {
+    if( i == commrank )
+      printf("%d %d %d %d %d\n",
+          phastarank,neighbors.size(),numtask,numowner,numtask-numowner);
+    MPI_Barrier(MPI_COMM_WORLD);
+  }
 
-  // Print now communication info
-  printf("\n");
-  printf("Communication info for this part:\n");
-  itkbeg = 0;
-  for(int itask=0;itask<numtask;itask++) {
-    int itag   = ilwork [itkbeg + 1];
-    int iacc   = ilwork [itkbeg + 2];
-    int iother = ilwork [itkbeg + 3];
-    int numseg = ilwork [itkbeg + 4];
-    int isgbeg = ilwork [itkbeg + 5];
+  if( verbosity > 0 ) {
+    // Print now communication info
+    printf("\n");
+    printf("Communication info for this part:\n");
+    itkbeg = 0;
+    for(int itask=0;itask<numtask;itask++) {
+      int itag   = ilwork [itkbeg + 1];
+      int iacc   = ilwork [itkbeg + 2];
+      int iother = ilwork [itkbeg + 3];
+      int numseg = ilwork [itkbeg + 4];
+      int isgbeg = ilwork [itkbeg + 5];
 
-    // Toal number of nodes involved in this communication (lfront), including all segments
-    int lfront = 0;
-    for(int is=1;is<=numseg;is++) {
-      int lenseg = ilwork [itkbeg + 4 + 2*is];
-      lfront = lfront + lenseg;
+      // Toal number of nodes involved in this communication (lfront), including all segments
+      int lfront = 0;
+      for(int is=1;is<=numseg;is++) {
+        int lenseg = ilwork [itkbeg + 4 + 2*is];
+        lfront = lfront + lenseg;
+      }
+      printf("Communication %d:\ttag %d\townership %d\trank %d\tnumseg %d\tnumvtx %d\n",itask,itag,iacc,iother,numseg,lfront);
+      itkbeg = itkbeg + 4 + 2*numseg;
     }
-    printf("Communication %d:\ttag %d\townership %d\trank %d\tnumseg %d\tnumvtx %d\n",itask,itag,iacc,iother,numseg,lfront);
-    itkbeg = itkbeg + 4 + 2*numseg;
+    printf("\n");
   }
 
   // Print now the raw ilwork array
-  printf("\n");
-  if( writeilwork ) {
+  if( verbosity > 1 ) {
     printf("ilwork array:\n");
     for(int i=0;i<len;i++) {
       printf("%d\n",ilwork[i]);
