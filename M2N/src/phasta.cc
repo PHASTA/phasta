afdb5118dfd2495aeb091efbcf4ec95116844359
Christian Rivera <riverc7@rpi.edu>
2016-09-28 13:06:27 -0400

    M2N: replaced deprecated header

--- M2N/src/phasta.cc
+++ M2N/src/phasta.cc
@@ -8,7 +8,7 @@
 
 #if !(defined IOSTREAMH)
 #include <iostream>
-#include <strstream>
+#include <sstream>
 using namespace std;
 #endif
 
@@ -91,7 +91,7 @@ phasta( int argc,
         if( gdb_child == 0 ) {
      
             cout << "Debugger Process initiating" << endl;
-            strstream exec_string;
+            stringstream exec_string;
          
 #if ( defined decalp )
             exec_string <<"xterm -e idb " 
@@ -109,7 +109,8 @@ phasta( int argc,
             exec_string <<"xterm -e dbx " 
                         << " -p "<< parent_pid <<" "<< argv[0] << endl;
 #endif
-            system( exec_string.str() );
+            string cmd = exec_string.str();
+            system( cmd.c_str() );
             exit(0);
         }
         catchDebugger();
